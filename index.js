b8065c27accae068c0b103db54455179a4d6c615
Vitor Silva <sirmochi45@gmail.com>
2018-04-07 23:32:17 -0300

    oauth

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = "https://surprisefb.herokuapp.com/"
 const port = process.env.PORT || 8080
 
 let express = require('express')
@@ -11,8 +12,15 @@ let app = express()
 // Configure express middlewares
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-surprise.use(express.static('cdn', options))
-
+app.use(express.static('cdn', {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())}}))
 
 // Configure Facebook API
 const fb_options = {
@@ -24,15 +32,47 @@ const fb_options = {
 
 FB.options(fb_options)
 
+let loginURL = (s) => {
+	let _path = path + "usertoken/" + s
+	return FB.getLoginUrl({
+  	scope: 'email,user_likes,user_birthday,user_friends',
+  	redirect_uri: _path})
+}
+
 // Configure DB
 db.connect('db', ['tokens', 'sessions', 'users'])
 
+// Helper Functions
+
+let isEmpty = (x) => {
+	for (i in x) {
+		return false;
+	}
+	return true;
+}
+
+let isValid = (x) => {
+	if(db.sessions.find({session: x.session})) return true;
+	return false;
+}
+
+// Server
+
 app.get('/', (req, res) => {
-	res.send('Hello World')
-})
+	if(isEmpty(req.cookies) || isValid(req.cookies)) {
+		res.redirect("/login")
+	} else {
+		res.redirect("/app")
+}})
 
 app.post('/new_session', (req, res) => {
-	db.count("sessions")
+	let session = db.sessions.count()
+	db.sessions.save({session: session, token:'', valid: true})
+	res.send(session.toString())
+})
+
+app.post('/oauth', (req, res) => {
+	res.send(loginURL(req.cookies.session))
 })
 
 app.listen(port, () => {
